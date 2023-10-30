ce2b527ae1bc976bc708aeaf27140b8eafd2e321
Jan Mayer <jan.demmerle@why.de>
2023-10-30 15:52:40 +0100

    feat: implement basic wine pairing app

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,8 @@ import WineList from "@/components/ui/wine-list";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full grid md:grid-cols-2">
-        {/* START menu */}
-        <div className="flex flex-col">
-          <div>
-            <h2>Menu</h2>
-          </div>
-          {/* START form */}
-          <WineList />
-          {/* END form */}
-        </div>
-        {/* END menu */}
-
-        {/* START wine list */}
-        <div>
-          <h2>Wine list</h2>
-        </div>
-        {/* START end list */}
-      </div>
+    <main className="flex h-screen flex-col items-center justify-between max-w-7xl mx-auto px-20 py-10">
+      <WineList />
     </main>
   )
 }
--- components/ui/wine-list.tsx
+++ components/ui/wine-list.tsx
@@ -1,29 +1,67 @@
 'use client'
 
 import { useChat } from 'ai/react'
+import { Input } from './input'
+import { Textarea } from './textarea'
+import { Button } from './button'
 
 export default function WineList() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
-    api: '/api/wine-list'
+    api: '/api/wine-list',
+    initialMessages: [
+      {
+        id: 'frame1',
+        role: 'system',
+        content:
+          'You are a master sommelier working in a Michelin-starred restaurant'
+      },
+      {
+        id: 'frame2',
+        role: 'system',
+        content:
+          'Pair the following dish with wine. Name a specific wine. Then, describe your pairing in a single sentence.'
+      }
+    ]
   })
 
   return (
-    <div>
-      <ul>
-        {messages.map((m, index) => (
-          <li key={index}>
-            {m.role === 'user' ? 'User: ' : 'AI: '}
-            {m.content}
-          </li>
-        ))}
-      </ul>
+    <div className="w-full flex-1 grid grid-cols-2 grid-rows-[1fr,auto]">
+      <div className="p-4 bg-gray-100 rounded-t-l-sm">
+        <h2 className="text-3xl font-medium mb-4">Menu</h2>
+        <ul className='divide-y-2'>
+          {messages
+            .filter(m => m.role === 'user')
+            .map((m, index) => (
+              <li className='py-4' key={index}>{m.content}</li>
+            ))}
+        </ul>
+      </div>
 
-      <form onSubmit={handleSubmit}>
-        <label>
-          Say something...
-          <input value={input} onChange={handleInputChange} />
-        </label>
-        <button type="submit">Send</button>
+      <div className="p-4 bg-slate-200 rounded-t-r-sm">
+        <h2 className="text-3xl font-medium mb-4">Wine List</h2>
+        <ul className='divide-y divide-slate-50'>
+          {messages
+            .filter(m => m.role === 'assistant')
+            .map((m, index) => (
+              <li className='py-4'  key={index}>{m.content}</li>
+            ))}
+        </ul>
+      </div>
+
+      <form
+        onSubmit={handleSubmit}
+        className="col-span-2 p-4 bg-slate-900 rounded-b-sm"
+      >
+        <div className="max-w-sm">
+          <Textarea
+            value={input}
+            onChange={handleInputChange}
+            placeholder="Add a dish"
+          />
+          <Button className="mt-4 bg-white text-slate-900" type="submit">
+            Pair 🍷
+          </Button>
+        </div>
       </form>
     </div>
   )
